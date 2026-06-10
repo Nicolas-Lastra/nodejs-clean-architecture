cbdb2e414f45b09e7580ff9ca96484c9ecc3c7a1
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2026-06-10 16:05:46 -0300

    The if (moviExists) statement was fixed, it was previously throwing an error when an empty array was returned from the repository.

--- services/movies/CreateMovieUseCase.js
+++ services/movies/CreateMovieUseCase.js
@@ -9,7 +9,7 @@ export class CreateMovieUseCase {
   async execute ({ title, year, director, duration, poster, rate, genres }) {
     const id = await this.idGenerator.generate()
     const movieExists = await this.moviesRepository.getByTitle(title)
-    if (movieExists || movieExists.length > 0) throw new Error('A movie with this title already exists')
+    if (movieExists && movieExists.length > 0) throw new Error('A movie with this title already exists')
 
     const movie = new Movie({ id, title, year, director, duration, poster, rate })
     const createdMovieWithGenres = await this.moviesRepository.create(movie, genres)
